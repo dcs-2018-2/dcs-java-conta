7b1519ed7572d9199b754664c034eae7094adff8
Prof. Ramon <ramon@ramon.pro.br>
2018-08-19 21:30:11 -0300

    Exercício preparado.

---
+++ src/main/java/br/pro/ramon/dcs/java/conta/ContaCorrente.java
@@ -0,0 +1,17 @@
+package br.pro.ramon.dcs.java.conta;
+
+public class ContaCorrente {
+
+    public double getSaldo() {
+        throw new UnsupportedOperationException("Escreva o método getSaldo().");
+    }
+
+    public void deposita(double valor) {
+        throw new UnsupportedOperationException("Escreva o método deposita().");
+    }
+
+    public void saque(double valor) {
+        throw new UnsupportedOperationException("Escreva o método saque()..");
+    }
+
+}
